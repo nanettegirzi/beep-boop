0951e03455369fd5ce1c866c134ca924329d493e
Nanette Girzi <ngirzi@gmail.com>
2018-01-19 16:12:39 -0800

    add more logic, still not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,38 @@
-$(document).ready(function() {
-  $("form#number-replacer").submit(function(event) {
-    event.preventDefault();
-
 var userInput = parseInt($("#number").val());
 
-var numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+var numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 
 var beep = "Beep";
 var boop = "Boop";
 var sorry = "I'm sorry. I'm afraid I can't do that.";
 
-for (var index = 0; index <=userInput; index)
-
 
 function beepBoop() {
-  if (numbers[i] % 3 === 0){
-
+  if (index % 3 === 0){
+    $("#list").append("<li>" + sorry + "</li>");
+  } else if (index.inludes(1)) {
+    $("#list").append("<li>" + boop + "</li>");
+  } else if (index.includes(0)) {
+    $("#list").append("<li>" + beep + "</li>");
   }
+};
+
+for (var index = 0; index <=userInput; index++) {
+  beepBoop();
 }
 
-});
+$(document).ready(function() {
+  $("#number-replacer").submit(function(event) {
+    event.preventDefault();
+
+    $("#number-replacer").toggle();
+    $("#result").toggle();
+
+    $("#again").click(function(event){
+        $("#number-replacer").toggle();
+        $("#result").toggle();
+      });
+
+
+  });
 });
